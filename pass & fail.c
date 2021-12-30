4ff0ba31672236802ac2a37d84d5facf3962f76d
Harikauppuluri <96563701+Harikauppuluri@users.noreply.github.com>
2021-12-30 20:22:15 +0530

    Add files via upload

---
+++ pass & fail.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+main()
+{
+	int a,b,c,d;
+	printf("enter a,b,c,d");
+	scanf("%d%d%d%d",&a,&b,&c,&d);
+	if(a>=35 && b>=35 && c>=35 && d>=35)
+	{
+		printf("pass");
+	}
+	else
+	{
+		printf("fail");
+	}
+}
